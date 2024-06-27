e589da587add92b796eec61b0286c0102e0c42f0
NamousNassim <nassimnamousdev@gmail.com>
2024-06-27 15:43:22 +0100

    eslintSetTtrue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+    eslint : 
+    {
+        ignoreDuringBuilds : true,
+    },
+
+};
 
 export default nextConfig;
